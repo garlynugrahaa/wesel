03c171d4283b429c02fb567873a096eb80bf5f69
garlynugrahaa <garlynugrahaa@gmail.com>
2024-05-29 00:07:42 +0700

    CONF : Package - RealRashid SweetAlert

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,6 +16,8 @@
     <link rel="stylesheet" href="{{ asset('vendor/stisla-2.2.0/dist/assets/css/components.css') }}">
 </head>
 <body>
+    @include('sweetalert::alert')
+    
     <div id="app">
         <div class="main-wrapper main-wrapper-1">
             <div class="navbar-bg"></div>
