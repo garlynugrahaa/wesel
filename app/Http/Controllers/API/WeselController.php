b080822df2167c41e299caf12cf9042c95f5e296
garlynugrahaa <garlynugrahaa@gmail.com>
2024-05-29 00:59:50 +0700

    FIX : API (Store) - Wesel

--- app/Http/Controllers/API/WeselController.php
+++ app/Http/Controllers/API/WeselController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\WeselResource;
 use App\Models\Wesel;
+use App\Models\WeselDetail;
 use Carbon\Carbon;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
@@ -16,29 +17,37 @@ class WeselController extends Controller
     {
         try {
             $Validator = FacadesValidator::make($Request->all(), [
+                'area' => 'required',
                 'voltage' => 'required',
                 'current'   => 'required',
                 'message'   => 'required',
                 'category'   => 'required',
-                'area' => 'required',
             ]);
     
             if ($Validator->fails()) {
                 return response()->json($Validator->errors(), 422);
             }
+            
+            $Wesel = Wesel::where('area', $Request->area)->get()->first();
 
-            $Wesel = Wesel::create([
-                'datetime' => Carbon::now(),
-                'voltage' => $Request->voltage,
-                'current' => $Request->current,
-                'message' => $Request->message,
-                'category' => $Request->category,
-                'area' => $Request->area,
-            ]);
+            if ($Wesel != NULL) {
+                WeselDetail::create([
+                    'wesel_id' => $Wesel->id,
+                    'voltage' => $Request->voltage,
+                    'current' => $Request->current,
+                ]);
+
+                $Wesel->update([
+                    'voltage' => $Request->voltage,
+                    'current' => $Request->current,
+                    'message' => $Request->message,
+                    'category' => $Request->category,
+                ]);
+            }
 
             return new WeselResource(true, 'You\'ve Successfully Registered', $Wesel);
         } catch (QueryException $e) {
-            
+            return new WeselResource(false, 'Error', $e);
         }
     }
 }
